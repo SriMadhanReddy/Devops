e661fe46f005cdab899d173abae0f16b870a791d
SriMadhanReddy <115637314+SriMadhanReddy@users.noreply.github.com>
2022-11-29 18:53:33 -0600

    Update envsetup.sh

--- django/scripts/envsetup.sh
+++ django/scripts/envsetup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ -d "env" ] 
-then
-    echo "Python virtual environment exists." 
-else
-    python3 -m venv env
-fi
-
-source env/bin/activate
 
 
 pip3 install -r /home/ec2-user/workspace/Django-pipeline/django/requirements.txt
